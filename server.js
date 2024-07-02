1abd0cd024553d208a608298b1ec60659f44ea00
lucianIrsigler <l.irsigler64@gmail.com>
2024-07-02 09:44:46 +0200

    Updated

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ function generateRoomCode() {
   return Math.random().toString(36).substring(2, 6).toUpperCase();
 }
 
-const PORT = process.env.PORT || 3009;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
