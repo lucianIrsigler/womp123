cc5b5ec04dfe46e6ca35d8d8793fa9df5be346a9
lucianIrsigler <l.irsigler64@gmail.com>
2024-07-03 11:06:33 +0200

    Updated

--- public/ball.js
+++ public/ball.js
@@ -239,9 +239,6 @@ function resetGame() {
 
   balls = [
     { column: 0, row: 0 },
-    { column: 9, row: 0 },
-    { column: 0, row: 8 },
-    { column: 9, row: 8 },
   ].map((ball) => ({
     x: ball.column * (wallW + pathW) + (wallW / 2 + pathW / 2),
     y: ball.row * (wallW + pathW) + (wallW / 2 + pathW / 2),
