03c94e8e1f570251f3356e6ffe2ead515c8364ee
AwesomeTevv <tevlen.naidoo@gmail.com>
2024-07-04 09:31:37 +0200

    Should be the latest

--- public/client.js
+++ public/client.js
@@ -14,11 +14,14 @@ const roomCodeInput = document.getElementById("room-code");
 const roomCodeDisplay = document.getElementById("room-code-display");
 const playerList = document.getElementById("player-list");
 const roomStatus = document.getElementById("room-status");
+const lobbyTitle = document.getElementById("lobby-title");
+const gyroContainer = document.getElementById("container");
 
 let currentRoom = null;
 let isHost = false;
 let gyroscopeInterval = null;
 const gyroscopeData = { alpha: 0, beta: 0, gamma: 0 };
+let globalMaze;
 
 let numRows = 10;
 let numCols = 10;
@@ -118,7 +121,7 @@ submitJoinBtn.addEventListener("click", () => {
   }
 });
 
-startGameBtn.addEventListener("click", () => {
+startGameBtn.addEventListener("click", (players) => {
   if (currentRoom) {
     if (isHost) {
       const roomCode = roomCodeDisplay.textContent.trim();
@@ -127,6 +130,11 @@ startGameBtn.addEventListener("click", () => {
       socket.emit("startGame", currentRoom);
     }
   }
+});
+socket.on("receieveMap", (maze) => {
+  console.log(maze);
+  globalMaze = maze;
+  console.log("MONEY BABY");
 });
 
 socket.on("roomCreated", (roomCode) => {
@@ -202,6 +210,20 @@ socket.on("gameStarted", (room) => {
       window.addEventListener("deviceorientation", handleOrientation);
       startSendingGyroscopeData();
     }
+  } else {
+    gyroContainer.style.display = "none";
+    playerList.style.display = "block";
+    lobbyTitle.style.display = "none";
+    roomStatus.style.display = "none";
+    startGameBtn.style.display = "none";
+    lobby.style.display = "block";
+    playerList.innerHTML = "";
+    globalMaze.room.players.forEach((player) => {
+      const li = document.createElement("li");
+      li.textContent = player.name;
+      li.style.color = colors[player.pid];
+      playerList.appendChild(li);
+    });
   }
 });
 
@@ -265,8 +287,11 @@ socket.on("gyroscopeUpdate", ({ playerId, data, room }) => {
 
 // Function to update the gyroscope display on the host screen
 function updateGyroscopeDisplay(playerId, data, room) {
-  document.body.style.backgroundColor =
-    softColors[room.players.findIndex((player) => player.id === socket.id)];
+  document.body.style.setProperty(
+    "background",
+    softColors[room.players.findIndex((player) => player.id === socket.id)],
+    "important"
+  );
   const playerElement = document.getElementById(`player-${playerId}`);
 
   if (!playerElement) {
